842d6d6d59e98cf835fb1f10a77808c51ab3461d
Alexander Voronkov <voronkovs.aleksadrs@gmail.com>
2023-05-24 19:38:50 +0300

    Added Victory package / Added Chart option to Details

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
-import React, { useEffect, useState } from "react"
+import React, { useEffect, useMemo, useState } from "react"
 import "./index.css"
 import { useAppContext } from "host/store"
+import { VictoryBar, VictoryChart, VictoryAxis, VictoryTheme } from "victory"
 
 const ForecastDay = ({ forecast }) => {
 	const { theme } = useAppContext()
@@ -37,31 +38,119 @@ const ForecastDay = ({ forecast }) => {
 	)
 }
 
+const ForecastChart = ({ forecastData }) => {
+	const parsedData = useMemo(() => {
+		return forecastData.map((forecast) => {
+			// const day = new Date(forecast.date).toLocaleDateString("en-US", {
+			// 	weekday: "long"
+			// })
+			// const averageTemp = forecast.day.avgtemp_c
+			return { date: forecast.date, temp: forecast.day.avgtemp_c }
+		})
+	}, [forecastData])
+
+	return (
+		<div
+			style={{
+				width: "100%",
+				height: "250px",
+				padding: 10
+			}}
+		>
+			<VictoryChart
+				height={200}
+				padding={30}
+				width={1150}
+				// adding the material theme provided with Victory
+				theme={VictoryTheme.material}
+				domainPadding={30}
+			>
+				<VictoryAxis
+					tickValues={parsedData.map((_, index) => index + 1)}
+					tickFormat={parsedData.map(({ date }) => date)}
+				/>
+				<VictoryAxis dependentAxis tickFormat={(x) => `${x}°C`} />
+				<VictoryBar data={parsedData} x="date" y="temp" />
+			</VictoryChart>
+		</div>
+	)
+}
+
 const Details = () => {
 	const { theme, selectedCity, fetchWeatherData } = useAppContext()
 	const [days, setDays] = useState([])
+	const [selectedOption, setSelectedOption] = useState("chart")
 
 	useEffect(() => {
 		if (selectedCity) {
-			fetchWeatherData(selectedCity).then((data) =>
-				setDays(data.forecast.forecastday)
-			)
+			fetchWeatherData(selectedCity).then((data) => {
+				if (selectedOption === "cards") {
+					const fiveDaysForecast = [...data.forecast.forecastday].splice(0, 5)
+					console.log("fiveDaysForecast", fiveDaysForecast)
+					setDays(fiveDaysForecast)
+				} else {
+					setDays(data.forecast.forecastday)
+				}
+			})
 		}
 		return () => {
 			setDays([])
 		}
-	}, [selectedCity])
+	}, [selectedCity, fetchWeatherData, selectedOption])
+
+	const cardsList = useMemo(() => {
+		return days.map((forecast, index) => (
+			<ForecastDay forecast={forecast} key={`forecast_${index}`} />
+		))
+	}, [days])
 
 	return (
 		<div className={`forecast_container forecast_container_${theme}`}>
 			{selectedCity && days.length && (
 				<>
-					<h2 className={`details_title_${theme}`}>Details</h2>
-					<div className="details_cards_container">
-						{days.map((forecast, index) => (
-							<ForecastDay forecast={forecast} key={`forecast_${index}`} />
-						))}
+					<div style={{ display: "flex", alignItems: "center" }}>
+						<h2
+							className={`details_title_${theme}`}
+							style={{ marginRight: 50 }}
+						>
+							Details
+						</h2>
+						<fieldset
+							style={{
+								display: "flex",
+								// width: 200,
+								justifyContent: "space-between"
+							}}
+						>
+							<legend>Select details view:</legend>
+							<div>
+								<input
+									type="radio"
+									id="chart"
+									name="drone"
+									value="chart"
+									onClick={() => setSelectedOption("chart")}
+									checked={selectedOption === "chart"}
+								/>
+								<label for="chart">chart</label>
+							</div>
+							<div>
+								<input
+									type="radio"
+									id="cards"
+									name="drone"
+									value="cards"
+									onClick={() => setSelectedOption("cards")}
+									checked={selectedOption === "cards"}
+								/>
+								<label for="cards">cards</label>
+							</div>
+						</fieldset>
 					</div>
+					{selectedOption === "cards" && (
+						<div className="details_cards_container">{cardsList}</div>
+					)}
+					{selectedOption === "chart" && <ForecastChart forecastData={days} />}
 				</>
 			)}
 		</div>
